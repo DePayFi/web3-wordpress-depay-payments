c674b48f29024cc6a8bf07173fccddf99bc13106
Sebastian Pape <s@pape.io>
2023-03-09 15:45:10 +0100

    v1.0.3

--- core/includes/classes/class-depay-payments-link.php
+++ core/includes/classes/class-depay-payments-link.php
@@ -12,7 +12,7 @@ class DePay_Payments_Link {
       DEPAYPAYMENTS_PLUGIN_URL . 'core/includes/assets/js/frontend.js',
       [],
       DEPAYPAYMENTS_PLUGIN_URL . 'core/includes/assets/js/frontend.js',
-      '1.0.2',
+      '1.0.3',
       true
     );
 
