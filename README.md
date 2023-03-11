5bf596d16b5b62f41e551c5f93c7ad1554a8fd03
Sebastian Pape <s@pape.io>
2023-03-11 11:26:37 +0100

    v1.1.0: deploy to wordpress.org

--- README.md
+++ README.md
@@ -21,6 +21,6 @@ https://web3wordpress.xyz/
 In order to release the plugins simply push a git tag:
 
 ```
-git tag v1.0.5
-git push origin v1.0.5
+git tag v1.1.0
+git push origin v1.1.0
 ```
--- core/includes/classes/class-depay-payments-link.php
+++ core/includes/classes/class-depay-payments-link.php
@@ -12,7 +12,7 @@ class DePay_Payments_Link {
       DEPAYPAYMENTS_PLUGIN_URL . 'core/includes/assets/js/frontend.js',
       [],
       DEPAYPAYMENTS_PLUGIN_URL . 'core/includes/assets/js/frontend.js',
-      '1.0.5',
+      '1.1.0',
       true
     );
 
