382b1525065c2c61a1667f13f2ceb859c9e93c30
Sebastian Pape <s@pape.io>
2023-10-23 14:19:36 +0200

    v2.1.1: fix wrongfully "failed" payments

--- README.md
+++ README.md
@@ -21,6 +21,6 @@ https://web3wordpress.xyz/
 In order to release the plugins simply push a git tag:
 
 ```
-git tag v2.1.0
-git push origin v2.1.0
+git tag v2.1.1
+git push origin v2.1.1
 ```
--- core/includes/classes/class-depay-payments-link.php
+++ core/includes/classes/class-depay-payments-link.php
@@ -12,7 +12,7 @@ class DePay_Payments_Link {
       DEPAYPAYMENTS_PLUGIN_URL . 'core/includes/assets/js/frontend.js',
       [],
       DEPAYPAYMENTS_PLUGIN_URL . 'core/includes/assets/js/frontend.js',
-      '2.1.0',
+      '2.1.1',
       true
     );
 
